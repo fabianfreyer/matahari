c05340e5f7de44b1747735c9a7feccfada54f385
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2015-07-08 00:11:06 +0200

    Moved modules config to config.yml

--- bootloader.mbr86/site_scons/config.py
+++ bootloader.mbr86/site_scons/config.py
@@ -5,16 +5,16 @@ import yaml
 
 debugger = debug.Bochs
 
-with open("modules.yml", "r") as stream:
-    modules = yaml.load(stream)
+with open("config.yml", "r") as stream:
+    configuration = yaml.load(stream)
 
 components = {
 	'mbr':	{
-		'modules': modules['mbr'],
+		'modules': configuration['modules']['mbr'],
 		'sections': ['.text_mbr', '.data_mbr', '.partition_table', '.sig']
 		},
 	'stage2': {
-		'modules': modules['stage2'],
+		'modules': configuration['modules']['stage2'],
 		'sections': ['.text_stage2', '.data_stage2']
 		}
 	}
